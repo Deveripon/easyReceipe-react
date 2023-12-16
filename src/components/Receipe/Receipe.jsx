96c4bf25cee52af6a209a227a3112c90c8a3a054
deveripon <developershahadathussainripon@gmail.com>
2023-12-16 11:35:22 +0600

    added api

--- src/components/Receipe/Receipe.jsx
+++ src/components/Receipe/Receipe.jsx
@@ -28,18 +28,19 @@ const Receipe = () => {
                                 />
                             </div>
                             <div className="receipe-content p-4 mb-4">
-                                <h2 className="receipe-title text-[24px]">{item.name}</h2>
+                                <Link className="" to={`/single/${item.id}`}>
+                                    <h2 className="receipe-title text-[24px]">{item.name}</h2>
+                                </Link>
+
                                 <div className="ingridients mb-4">
                                     <h3 className="text-primary text-2xl">Ingridients:</h3>
                                     <p className="text-gray-600 text-[17px] font-paragraphFont mt-2">
                                         {item.ingridients}
                                     </p>
-                                    <h3 className="text-primary text-2xl mt-4">Receipe:</h3>
-                                    <p className="text-gray-600 text-[17px] font-paragraphFont mt-2">
-                                        {item.receipe}
-                                    </p>
                                 </div>
-                                <Link className="primary-button py-2 px-3" to="/single/id">
+                                <Link
+                                    className="primary-button py-2 px-3"
+                                    to={`/single/${item.id}`}>
                                     Show Full Receipe
                                 </Link>
                             </div>
--- src/pages/Single/Single.jsx
+++ src/pages/Single/Single.jsx
@@ -1,4 +1,19 @@
+import { useLocation } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Single = () => {
+    const location = useLocation();
+    const [data, setData] = useState({});
+    useEffect(() => {
+        axios
+            .get("http://localhost:5050/receipe/" + location.pathname.at(-1))
+            .then((res) => {
+                setData(res.data);
+            })
+            .catch((err) => {});
+    }, []);
+    console.log(data);
     return (
         <>
             <main className="m-auto w-[calc(80%)]">
@@ -6,31 +21,21 @@ const Single = () => {
                     <div className="receipe-img">
                         <img
                             className="rounded w-[calc(98%)] max-h-[60vh] m-auto object-cover"
-                            src="https://images.squarespace-cdn.com/content/v1/5d5577ab9ce6c9000153aa4f/ad700fc6-04d6-49be-8005-96dc2a8ae189/Final+Dish.jpg?format=750w"
+                            src={data.photo}
                             alt="receipe-img"
                         />
                     </div>
                     <div className="receipe-review py-5 flex justify-center items-center">
                         <div className="receipe-content p-4 mb-4 w-[60%]">
-                            <h2 className="receipe-title text-[24px]">
-                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora,
-                                laboriosam.
-                            </h2>
+                            <h2 className="receipe-title text-[24px]">{data.name}</h2>
                             <div className="ingridients mb-4">
                                 <h3 className="text-primary text-2xl">Ingridients:</h3>
                                 <p className="text-gray-600 text-[17px] font-paragraphFont mt-2">
-                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                                    Recusandae at rerum molestiae nobis adipisci natus eaque earum
-                                    blanditiis enim architecto! Lorem ipsum dolor sit amet
-                                    consectetur adipisicing elit. Et, libero.
+                                    {data.ingridients}
                                 </p>
                                 <h3 className="text-primary text-2xl mt-4">Receipe:</h3>
                                 <p className="text-gray-600 text-[17px] font-paragraphFont mt-2">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-                                    necessitatibus recusandae obcaecati ducimus dolorem deserunt
-                                    fugit eum repudiandae, tenetur repellat quo? A exercitationem
-                                    rem dolorum sed blanditiis repellendus amet. Quas facilis in
-                                    beatae amet quod distinctio inventore nostrum, corporis aperiam?
+                                    {data.receipe}
                                 </p>
                             </div>
                         </div>
@@ -67,6 +72,7 @@ const Single = () => {
                     </div>
                 </div>
             </main>
+            ;
         </>
     );
 };
